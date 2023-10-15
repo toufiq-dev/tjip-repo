7289804321dc626ee053037b78ac262de8cb8f78
toufiq-dev <toufiq.dev@gmail.com>
2023-10-15 11:34:00 +0600

    optimized majority element from SC O(N) to O(1)

--- week01/majority-element.cpp
+++ week01/majority-element.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 class Solution {
 public:
+    // Brute-force
     // TC: O(N)
     // SC: O(N)
     int majorityElement(vector<int>& nums) {
@@ -23,6 +24,41 @@ public:
 
         return -1;
     }
+
+    // Optimal solution
+    // TC: O(N)
+    // SC: O(1)
+    int majorityElement2(vector<int>& nums) {
+        int candidate = -1, votes = 0;
+
+        // find majority candidate
+        for(int i = 0; i < nums.size(); i++) {
+            if(votes == 0) {
+                candidate = nums[i];
+                votes = 1;
+            } else {
+                if(nums[i] == candidate) {
+                    votes++;
+                } 
+                else 
+                    votes--;
+            }
+        }
+        
+        // find if majority candidate appears more than ⌊n / 2⌋ times.
+        int count = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == candidate)
+                count++;
+        }
+
+        if(count > floor(nums.size() / 2)) 
+            return candidate;
+
+        return -1;
+    }
+
 };
 
 int main() {
@@ -30,7 +66,7 @@ int main() {
 
     Solution sln;
 
-    int ans = sln.majorityElement(arr);
+    int ans = sln.majorityElement2(arr);
 
     cout << ans << endl;
 
