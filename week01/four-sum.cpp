38c33a6ef8eaa009f5d4cede8d265a354722b0cc
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2023-10-18 18:54:08 +0600

    Update four-sum.cpp

--- week01/four-sum.cpp
+++ week01/four-sum.cpp
@@ -4,49 +4,49 @@
 using namespace std;
 
 class Solution {
-public:
-    // TC: O(N ^ 3)
-    // SC: O(N)
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        // step1: sort the array
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> quadruplets;
+    public:
+        // TC: O(N ^ 3)
+        // SC: O(N)
+        vector<vector<int>> fourSum(vector<int>& nums, int target) {
+            // step1: sort the array
+            sort(nums.begin(), nums.end());
+            vector<vector<int>> quadruplets;
 
-        // step2: apply three sum on each element
-        for(int i = 0; i < nums.size(); ++i) {
-            for(int j = i + 1; j < nums.size(); ++j) {
-                int a = nums[i] + nums[j];
-                int L = j + 1;
-                int R = nums.size() - 1;
-                long long newTarget = static_cast<long long>(target) - a;
+            // step2: apply three sum on each element
+            for(int i = 0; i < nums.size(); ++i) {
+                for(int j = i + 1; j < nums.size(); ++j) {
+                    int a = nums[i] + nums[j];
+                    int L = j + 1;
+                    int R = nums.size() - 1;
+                    long long newTarget = static_cast<long long>(target) - a;
 
-                while(L < R) {
-                    if(nums[L] + nums[R] < newTarget)
-                        L++;
-                    else if(nums[L] + nums[R] > newTarget) 
-                        R--;
-                    else {
-                        while(L + 1 < R && nums[L] == nums[L + 1])
+                    while(L < R) {
+                        if(nums[L] + nums[R] < newTarget)
                             L++;
-                        while(L < R - 1 && nums[R] == nums[R - 1])
+                        else if(nums[L] + nums[R] > newTarget) 
                             R--;
+                        else {
+                            while(L + 1 < R && nums[L] == nums[L + 1])
+                                L++;
+                            while(L < R - 1 && nums[R] == nums[R - 1])
+                                R--;
 
-                        quadruplets.push_back({nums[i], nums[j], nums[L], nums[R]});
-                        L++;
-                        R--;
+                            quadruplets.push_back({nums[i], nums[j], nums[L], nums[R]});
+                            L++;
+                            R--;
+                        }
                     }
+                    
+                    while(j + 1 < nums.size() && nums[j] == nums[j + 1])
+                        j++;
                 }
-                
-                while(j + 1 < nums.size() && nums[j] == nums[j + 1])
-                    j++;
+
+                while(i + 1 < nums.size() && nums[i] == nums[i + 1])
+                    i++;
             }
 
-            while(i + 1 < nums.size() && nums[i] == nums[i + 1])
-                i++;
+            return quadruplets;
         }
-
-        return quadruplets;
-    }
 };
 
 int main() {
