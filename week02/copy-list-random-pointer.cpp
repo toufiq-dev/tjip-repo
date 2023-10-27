9b1f1f0841563c0c0e1e3ac2d2f33ee8a1659215
toufiq-dev <toufiq.dev@gmail.com>
2023-10-27 23:45:29 +0600

    copy list random pointer

---
+++ week02/copy-list-random-pointer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // TC: O(N)
+    // SC: O(N)
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+
+        unordered_map<Node*, Node*> nodeMap;
+
+        Node* current = head;
+        while (current) {
+            nodeMap[current] = new Node(current -> val);
+            current = current -> next;
+        }
+        
+        current = head;
+        while (current) {
+            Node* copyNode = nodeMap[current];
+            copyNode->next = nodeMap[current->next];
+            copyNode->random = nodeMap[current->random];
+            current = current->next;
+        }
+
+        return nodeMap[head];
+    }
+};
