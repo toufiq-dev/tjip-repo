713b9e419523714d9e1fe2ee6c0d77cfe1d931dd
toufiq-dev <toufiq.dev@gmail.com>
2023-11-02 13:08:02 +0600

    feat: longest common prefix

--- week03/longest-common-prefix.cpp
+++ week03/longest-common-prefix.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
   string longestCommonPrefix(vector<string>& strs) {
-    string prefix = strs[0];
-    for (const string &str : strs) {
+    auto prefix = strs[0];
+    for (auto &str : strs) {
       string new_prefix = "";
       for (int i = 0; i < min(prefix.size(), str.size()); i++) {
         if (prefix[i] == str[i]) {
