e629b78d65ef1007752fdb8ba4343b42a14a48b6
toufiq-dev <toufiq.dev@gmail.com>
2023-11-04 20:41:06 +0600

    fix: tc and sc of longest-common-prefix

--- week03/longest-common-prefix.cpp
+++ week03/longest-common-prefix.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+  // TC: O(m * n) where m = |strs|, n = |prefix| / |str|
+  // SC: O(n) where n = longest commong prefix
   string longestCommonPrefix(vector<string>& strs) {
     auto prefix = strs[0];
+
     for (auto &str : strs) {
       string new_prefix = "";
+
       for (int i = 0; i < min(prefix.size(), str.size()); i++) {
         if (prefix[i] == str[i]) {
           new_prefix += prefix[i];
