7875444fa72328b31e631456af4158df355c253c
toufiq-dev <toufiq.dev@gmail.com>
2023-12-03 21:19:18 +0600

    new: firstBadVersion

---
+++ week05/firstBadVersion.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int L = 1, R = n;
+        while(L < R) {
+            int mid = L + (R - L) / 2;
+            if(!isBadVersion(mid)) 
+                L = mid + 1;
+            else 
+                R = mid;
+        }
+
+        return L;
+    }
+};
