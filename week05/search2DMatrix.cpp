529ccddb146725fd4c2b867f0df68b975715caf1
toufiq-dev <toufiq.dev@gmail.com>
2023-12-04 20:01:50 +0600

    new: search2DMatrix

---
+++ week05/search2DMatrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    // TC: O(log(m * n)), m x n integer matrix
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int L = 0;
+        int R = (m * n) - 1;
+
+        while(L <= R) {
+            int M = L + (R - L) / 2;
+            int x = M / n;
+            int y = M % n;
+            if(matrix[x][y] < target)
+                L = M + 1;
+            else if (matrix[x][y] > target) 
+                R = M - 1;
+            else 
+                return true;
+        }
+
+        return false;
+    }
+};
