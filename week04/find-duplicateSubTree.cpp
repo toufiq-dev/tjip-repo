10f787c1823983239d9b391566d94e5fa88aedfa
toufiq-dev <toufiq.dev@gmail.com>
2023-12-06 19:31:17 +0600

    fix: pr

---
+++ week04/find-duplicateSubTree.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // TC: O(n), n = no. of nodes
+    // SC: O(n)
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        unordered_map<string, int> subtreeCount;  
+        unordered_set<TreeNode*> result;  
+        postorder(root, subtreeCount, result);
+        return vector<TreeNode*>(result.begin(), result.end());
+    }
+
+private:
+    string postorder(TreeNode* root, unordered_map<string, int>& subtreeCount, unordered_set<TreeNode*>& result) {
+        if (!root) return "#";  
+
+        string left = postorder(root->left, subtreeCount, result);
+        string right = postorder(root->right, subtreeCount, result);
+
+        string subtree = to_string(root->val) + "," + left + "," + right;
+
+        ++subtreeCount[subtree];
+
+        if (subtreeCount[subtree] == 2) 
+            result.insert(root);
+
+        return subtree;
+    }
+};
