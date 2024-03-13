7c512b3f5e7550751c58cb409199561e744314cf
toufiq-dev <toufiq.dev@gmail.com>
2024-03-14 01:12:47 +0600

    Subsets

---
+++ week09/subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // TC: O(2^N) N = |nums|
+    // SC: O(N)
+    vector<vector<int>> subsets(vector<int>& nums) {
+        subset.clear();
+        result.clear();
+        gen(nums, 0);
+        return result;
+    }
+private:
+    vector<int> subset;
+    vector<vector<int>> result;
+
+    void gen(vector<int>& nums, int idx) {
+        if (idx >= nums.size()) {
+            result.push_back(subset);
+            return;
+        }
+
+        // left call - include
+        subset.push_back(nums[idx]);
+        gen(nums, idx + 1);
+        subset.pop_back();
+
+        // right call - skip
+        gen(nums, idx + 1);
+    }
+};
